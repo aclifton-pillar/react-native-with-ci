de35b52554c79c8ef32d8dfc492fe728adde459b
Charles Hoffman <charles.d.hoffman@accenture.com>
2019-10-30 09:46:56 -0500

    ye simple websocket server

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const moment = require('moment');
+
+app.use(bodyParser());
+
+const port = 3000;
+
+const SocketServer = require('./src/SocketServer');
+
+let socketServer;
+
+app.get('/ping', (req, res) => {
+  res.send('pong');
+});
+
+app.get('/time', (req, res) => {
+  const currentTime = moment();
+
+  socketServer.sendTime();
+
+  res.send(`${JSON.stringify(currentTime)}`);
+});
+
+let server = app.listen(port, () => console.log(`Listening at port ${port}`));
+
+socketServer = new SocketServer(server);
+
+module.exports = {app, server};
---
+++ server/src/SocketServer.js
@@ -0,0 +1,40 @@
+const WebSocket = require('ws');
+const moment = require('moment');
+
+class SocketServer {
+  constructor(server) {
+    this._timeClients = [];
+    this._wss = new WebSocket.Server({noServer: true});
+    this._wss.on('connection', (ws, request) => {
+      this.registerClient(ws);
+    });
+    if (server) {
+      this.register(server);
+    }
+  }
+
+  register(server) {
+    server.on('upgrade', (request, socket, head) => {
+      this._wss.handleUpgrade(request, socket, head, (ws) => {
+        this._wss.emit('connection', ws, request);
+      });
+    });
+  }
+
+  disconnect(sender) {
+    return () => {
+      this._timeClients = this._timeClients.filter(client => client !== sender);
+    }
+  }
+
+  sendTime() {
+    this._timeClients.forEach(client => client.send(moment().toString()));
+  }
+
+  registerClient(ws, name) {
+    ws.on('close', this.disconnect(ws));
+    this._timeClients.push(ws);
+  }
+}
+
+module.exports = SocketServer;
