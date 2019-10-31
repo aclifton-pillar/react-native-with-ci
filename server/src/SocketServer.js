b7a9792f0c6b38b4314df28f4cf7c4127b9626d4
Charles Hoffman <charles.d.hoffman@accenture.com>
2019-10-31 13:20:12 -0500

    reformatted 2 sp to 4

--- server/src/SocketServer.js
+++ server/src/SocketServer.js
@@ -2,43 +2,43 @@ const WebSocket = require('ws');
 const moment = require('moment');
 
 class SocketServer {
-  constructor(server) {
-    this._timeClients = [];
-    this._wss = new WebSocket.Server({noServer: true});
-    this._wss.on('connection', (ws, request) => {
-      this.registerClient(ws);
-    });
-    if (server) {
-      this.register(server);
+    constructor(server) {
+        this._timeClients = [];
+        this._wss = new WebSocket.Server({noServer: true});
+        this._wss.on('connection', (ws, request) => {
+            this.registerClient(ws);
+        });
+        if (server) {
+            this.register(server);
+        }
     }
-  }
 
-  register(server) {
-    server.on('upgrade', (request, socket, head) => {
-      this._wss.handleUpgrade(request, socket, head, (ws) => {
-        this._wss.emit('connection', ws, request);
-      });
-    });
-      setInterval(this.sendTime.bind(this), 1000);
-  }
-
-  disconnect(sender) {
-    return () => {
-      this._timeClients = this._timeClients.filter(client => client !== sender);
+    register(server) {
+        server.on('upgrade', (request, socket, head) => {
+            this._wss.handleUpgrade(request, socket, head, (ws) => {
+                this._wss.emit('connection', ws, request);
+            });
+        });
+        setInterval(this.sendTime.bind(this), 1000);
     }
-  }
 
-  sendTime() {
-    this._timeClients.forEach(client => client.send(
-	JSON.stringify({time: moment().toString()})));
-  }
+    disconnect(sender) {
+        return () => {
+            this._timeClients = this._timeClients.filter(client => client !== sender);
+        }
+    }
 
-  registerClient(ws, name) {
-    console.log('connected client');
-    ws.on('close', this.disconnect(ws));
-    ws.on('message', _ => console.dir(_));
-    this._timeClients.push(ws);
-  }
+    sendTime() {
+        this._timeClients.forEach(client => client.send(
+            JSON.stringify({time: moment().toString()})));
+    }
+
+    registerClient(ws, name) {
+        console.log('connected client');
+        ws.on('close', this.disconnect(ws));
+        ws.on('message', console.dir);
+        this._timeClients.push(ws);
+    }
 }
 
 module.exports = SocketServer;
